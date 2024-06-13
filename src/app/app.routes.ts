3065cc3445da72bf5afbb31e674b4d979455b447
niteshnandak <140968896+niteshnandak@users.noreply.github.com>
2024-06-13 21:48:37 +0530

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { RouterModule, Routes } from '@angular/router';
-import { PeopleComponent } from './people/people.component';
-
-export const routes: Routes = [
-    { path: 'people', component: PeopleComponent}
-];
